a6a26ada6242bfe3909780b4bec9a325f84561e3
Yaroslava <you@example.com>
2024-08-19 20:38:38 +0300

    refactoring class User

--- include/user.h
+++ include/user.h
@@ -12,36 +12,7 @@ struct GECOS {
     std::string m_homemobile;
 
     GECOS() = default;
-
-    GECOS(const std::string &gecos_str)
-    : m_fullname(), m_address(), m_workmobile(), m_homemobile()
-    {
-        size_t begin = 0;
-        size_t pos = 0;
-        pos = std::count(gecos_str.begin(), gecos_str.end(), ',');
-        if (pos != 3) {
-            m_fullname = gecos_str;
-        } else {
-            //=========m_fullname==========
-            pos = gecos_str.find(',', begin);
-            m_fullname = gecos_str.substr(begin, pos - begin);
-            begin = pos + 1;
-
-            //=========m_address==========
-            pos = gecos_str.find(',', begin);
-            m_address = gecos_str.substr(begin, pos - begin);
-            begin = pos + 1;
-
-            //=========m_workmobile==========
-            pos = gecos_str.find(',', begin);
-            m_address = gecos_str.substr(begin, pos - begin);
-            begin = pos + 1;
-
-            //=========m_homemobile==========
-            pos = gecos_str.find(',', begin);
-            m_homemobile = gecos_str.substr(begin, pos - begin);
-        }
-    }
+    GECOS(const std::string &gecos_str);
 };
 
 class User {
@@ -53,5 +24,7 @@ private:
     std::string m_homedir_path;
     std::string m_interpretator;
 public:
+    User()=default;
+    User(const std::string &str);
     friend std::istream &operator>>(std::istream &in, User &user);
 };
