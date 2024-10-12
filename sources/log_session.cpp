716eb24fdd4330bd02dd526046e71e2e9a83a2e0
Yaroslava <you@example.com>
2024-10-12 19:00:20 +0300

    New

--- sources/log_session.cpp
+++ sources/log_session.cpp
@@ -6,16 +6,31 @@ Date::Date(const std::string &str) {
 
 }
 
-Date::Duration::Duration(const std::string &str) {
-    auto begin = str.begin();
-    if (auto pos = find(begin, str.end(), '+'); pos != str.end()) {
-        std::string day{str.begin(), pos};
-        m_day = std::strtol(day.data(), nullptr, 10);
-        begin = ++pos;
+Date::Duration::Duration(const std::string &str)
+:m_day{}, m_hour{}, m_minutes{}
+{
+    if(str.find_first_not_of("+:0123456789") != std::string::npos || !std::isdigit(str[0]) || !std::isdigit(str.back())) throw std::exception();
+    int ret = 0;
+    ret = sscanf(str.data(), "%hd+%hd:%hd", &m_day, &m_hour, &m_minutes);
+    if (ret != 3) {
+        ret = sscanf(str.data(), "%hd:%hd", &m_hour, &m_minutes);
+        if (ret != 2)
+            throw std::exception();
     }
-    // begin в любом случае является точкой, где начинаются часы...
 
-
+//    auto begin = str.begin();
+//    if (auto pos = find(begin, str.end(), '+'); pos != str.end()) {
+//        std::string day{str.begin(), pos};
+//        m_day = std::strtol(day.data(), nullptr, 10);
+//        begin = ++pos;
+//    }
+////    std::string hour{begin,begin+2};
+////    m_hour = std::strtol(hour.data(), nullptr, 10);
+////    std::string minute{begin+3,begin+5};
+////    m_minutes = std::strtol(minute.data(), nullptr, 10);
+//
+//    std::string h_m{begin, str.end()};
+//    sscanf(h_m.data(), "%hd:%hd", &m_hour, &m_minutes);
 }
 
 LogSession::LogSession(const std::string &str)
